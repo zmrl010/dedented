b75f9ce043174b4931d6a72f061636625d9b6b00
Zach <zmriel@gmail.com>
2023-04-12 17:32:42 -0400

    test: add test suite for dedent

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-export { };
+/**
+ * Remove leading indentation from multi-line string while preserving
+ * visual consistency with the original string.
+ *
+ * @param templateStrings - template literal(s) to interpolate
+ * @param args - 0 or more arguments that are results of expressions
+ * interpolated within the template strings (using `${expression}`)
+ *
+ * @returns string with indentation stripped
+ */
+export function dedent(
+  templateStrings: TemplateStringsArray | string,
+  ...args: unknown[]
+): string {
+  return "";
+}
